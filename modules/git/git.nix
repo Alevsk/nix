470f715bf7c06113589360ed9edd5fbe3b45a5a6
Lenin Alevski <alevsk.8772@gmail.com>
2025-08-24 21:12:32 -0700

    feat: enable git commit signing with 1Password SSH key

--- modules/git/git.nix
+++ modules/git/git.nix
@@ -5,11 +5,17 @@
     enable = true;
     userName = "Lenin Alevski";
     userEmail = "alevsk.8772@gmail.com";
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNo+JZ6HwtwIkjEcFsdvuumgoKSMIYri54g5AwGT4/j";
+      signByDefault = true;
+    };
     extraConfig = {
       init.defaultBranch = "main";
       push.default = "simple";
       pull.rebase = true;
       core.editor = "nvim";
+      gpg.format = "ssh";
+      "gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
     };
   };
 }
